d22add48e1dc646cdf15ae343eb5e4ccb96d30c1
lee910814 <62333447+lee910814@users.noreply.github.com>
2021-10-12 20:50:26 +0900

    Add files via upload
    buchekList추가

---
+++ todo.js
@@ -0,0 +1,92 @@
+const todoForm = document.getElementById("form");
+const todoInput = todoForm.querySelector("input");
+const todoList = document.querySelector(".list");
+const todoSpan = document.querySelector(".error");
+const allBtn = document.querySelector("#allBtn");
+const clock = document.querySelector(".clock");
+
+let toDos =  [];
+
+const TODO_KEY = "toDos"
+
+function saveLocal(){
+    localStorage.setItem(TODO_KEY,JSON.stringify(toDos)); //localstorage에 데이터저장
+}
+
+function deleteToDo(e){
+    const parentList = e.target.parentElement;
+    localStorage.removeItem(parentList);
+    parentList.remove(); //부모요소 삭제
+}
+
+//시계
+function homeClock(){
+    const span = document.createElement("span");
+    let date = new Date();
+    let hours = date.getHours();
+    let Minites = date.getMinutes();
+    let seconds = String(date.getSeconds());
+    clock.appendChild(span);
+    clock.innerText = hours + ":"+ Minites + ":" + seconds;
+    if(clock >= 12){
+        span.innerText ="PM";
+    }else{
+        span.innerText = "AM";
+    }
+}
+
+setInterval(homeClock,1000);
+
+
+
+
+//리스트 추가
+function addList(newTodo){
+    const li = document.createElement("li");
+    const button = document.createElement("button");
+    const button1 = document.createElement("button");
+    const span = document.createElement("span");
+
+    //에러문
+    if(!newTodo){
+        todoSpan.innerHTML = "입력을 깜빡한 것 같아요"
+    }else{
+    span.innerHTML = newTodo;
+    todoList.appendChild(li);
+    li.appendChild(button1);
+    li.appendChild(span);
+    li.appendChild(button);
+     todoSpan.innerHTML = "";
+     button.className = "far fa-trash-alt" //삭제버튼
+     button1.className = "fas fa-check" //체크버튼
+     button1.addEventListener("click",function(){
+         span.style.textDecoration="line-through";
+         button1.style.color="red";
+
+     });
+     button.addEventListener("click",deleteToDo);
+}
+}
+
+//폼에 입력될때
+function saveForm(e){
+    e.preventDefault();
+    const newTodo = todoInput.value;
+    todoInput.value = "";
+    addList(newTodo);
+    toDos.push(newTodo);
+    saveLocal();
+}
+
+todoForm.addEventListener("submit",saveForm);
+allBtn.addEventListener("click",allBtn);
+
+const localTodo = localStorage.getItem(TODO_KEY);
+
+
+if(localTodo !== null){
+    const parsed = JSON.parse(localTodo);
+
+    toDos = parsed;
+    parsed.map(addList);
+}
